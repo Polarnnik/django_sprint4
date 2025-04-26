2886bd426c60e3165a57c48c9a723edb1dd43c5f
Polarnnik <rostislav.voloshin@yandex.ru>
2025-04-26 16:55:20 +0700

    Добавление изображений к постам и небольшие изминения

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -72,6 +72,11 @@ class Post(BaseModel):
         verbose_name='Категория',
         related_name='posts'
     )
+    image = models.ImageField(
+            'Изображение',
+            upload_to='posts_images',
+            blank=True
+    )
 
     class Meta:
         verbose_name = 'публикация'
