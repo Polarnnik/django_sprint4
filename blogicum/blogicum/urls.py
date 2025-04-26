87e1029b8f54166d105ef7890ff3878db198ed85
Polarnnik <rostislav.voloshin@yandex.ru>
2025-04-26 15:11:14 +0700

    Начальная настройка

---
+++ blogicum/blogicum/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('', include('blog.urls')),
+    path('pages/', include('pages.urls')),
+    path('admin/', admin.site.urls),
+]
+
+handler404 = 'pages_views.page_not_found'
+handler500 = 'pages_views.server_error'
