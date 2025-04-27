c6ca0c49e540e6fe8170940f3d02b23a49d3adeb
Polarnnik <rostislav.voloshin@yandex.ru>
2025-04-27 14:23:14 +0700

    Починины страницы с ошибками

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
 from django.urls import include, path
-from pages import views as pages_views
+from django.conf import settings
+from django.conf.urls.static import static
+from blog.views import SignUp
 
 urlpatterns = [
+    path('auth/registration/', SignUp.as_view(), name='signup'),
     path('', include('blog.urls')),
     path('pages/', include('pages.urls')),
     path('admin/', admin.site.urls),
     path('auth/', include('django.contrib.auth.urls')),
 ]
 
-handler404 = pages_views.page_not_found
-handler500 = pages_views.server_error
+handler404 = 'pages.views.page_not_found'
+handler500 = 'pages.views.server_error'
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
